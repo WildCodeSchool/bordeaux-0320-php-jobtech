e0d847dba690a97c8b0f6b24c0294d6a2f3eca54
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-05-29 18:48:27 +0200

    Stupid Travis

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,9 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// Font Awesome
+import '@fortawesome/fontawesome-free/js/all';
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
 
@@ -13,6 +16,3 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-
-// Font Awesome
-import '@fortawesome/fontawesome-free/js/all'
