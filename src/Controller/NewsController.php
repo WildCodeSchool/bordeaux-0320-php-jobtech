85e68abc0ed6033a019f1ab67234add0c77e1c2f
TimoArdi <t.ardilouze@gmail.com>
2020-06-03 15:41:11 +0200

    Add news page and NewsController

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\News;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewsController extends AbstractController
+{
+    /**
+     * @Route("/news", name="news_list")
+     * @return Response A response instance
+     */
+    public function list(): Response
+    {
+        $news = $this->getDoctrine()
+            ->getRepository(News::class)
+            ->findBy([], ['createdOn' => 'DESC'], 5, 0);
+
+        if (!$news) {
+            throw $this->createNotFoundException(
+                'No news found in news table.'
+            );
+        }
+        return $this->render(
+            'job_tech/news/list.html.twig',
+            ['news' => $news]
+        );
+    }
+}
