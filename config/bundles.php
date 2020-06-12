7753d15f1952772ed1cb81b8e155a8b563058cc8
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-06-12 02:30:27 +0200

    Add CkEditor and change in easy admin bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
