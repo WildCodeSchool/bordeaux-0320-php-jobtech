81fbef90f59a9c5c3dcf4f962e96e9ccbfb2d05a
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-06-14 17:11:07 +0200

    Add carousel index

--- src/Controller/JobTechController.php
+++ src/Controller/JobTechController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\JobCategoryRepository;
+use App\Repository\NewsRepository;
 use App\Repository\OfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,18 +12,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class JobTechController extends AbstractController
 {
     const MAX_JOB_CATEGORY_IN_INDEX = 10;
+    const MAX_NEWS_IN_CAROUSEL = 3;
     const MAX_OFFER_IN_INDEX = 6;
 
     /**
      * @Route("/", name="index")
      * @param JobCategoryRepository $jobCategoryRepo
      * @param OfferRepository $offerRepository
+     * @param NewsRepository $newsRepository
      * @return Response
      */
-    public function index(JobCategoryRepository $jobCategoryRepo, OfferRepository $offerRepository): Response
-    {
+    public function index(
+        JobCategoryRepository $jobCategoryRepo,
+        OfferRepository $offerRepository,
+        NewsRepository $newsRepository
+    ): Response {
         return $this->render('index.html.twig', [
             'job_categories' => $jobCategoryRepo->getJobCategoryWithOffersNb(self::MAX_JOB_CATEGORY_IN_INDEX),
+            'actualities' => $newsRepository->findBy([], ['createdAt' => 'DESC'], self::MAX_NEWS_IN_CAROUSEL),
             'offers' => $offerRepository->findByAndAddInterval([], ['createdAt' => 'DESC'], self::MAX_OFFER_IN_INDEX)
         ]);
     }
