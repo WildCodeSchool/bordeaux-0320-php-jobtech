79387468871394aaf5ec81c792e0c85458a63d26
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-06-23 14:38:08 +0200

    Candidate profile

---
+++ src/Service/NumberProcessing.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+class NumberProcessing
+{
+    public static function addPointToPhoneNumber(int $phone)
+    {
+        $phone = '0' . $phone;
+        $result = '';
+        for ($i = 0; $i < 10; $i++) {
+            if ($i === 2 || $i == 4 || $i == 6 || $i == 8) {
+                $result .= '.';
+            }
+            $result .= $phone[$i];
+        }
+        return $result;
+    }
+}
