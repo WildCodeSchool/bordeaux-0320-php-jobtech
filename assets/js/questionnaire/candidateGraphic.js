30bf55d6efd98c738865e177506562c339b506da
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-07-07 23:39:37 +0200

    fix problem travis

--- assets/js/questionnaire/candidateGraphic.js
+++ assets/js/questionnaire/candidateGraphic.js
@@ -2,28 +2,21 @@ import Chart from 'chart.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const url = document.getElementById('candidate').getAttribute('data-url');
-    let pro = null;
-    let perso = null;
-    let postedAt = null;
     fetch(url)
         .then((response) => {
             response.json()
                 .then((data) => {
-                    pro = data.pro;
-                    perso = data.perso;
-                    postedAt = data.date;
-
-                    if (pro) {
+                    if (data.pro) {
                         const ctxPro = document.getElementById('myChartPro')
                             .getContext('2d');
                         const myChartPro = new Chart(ctxPro, {
                             type: 'radar',
                             data: {
-                                labels: pro.ability,
+                                labels: data.pro.ability,
                                 datasets: [
                                     {
                                         label: 'Notes',
-                                        data: pro.score,
+                                        data: data.pro.score,
                                         backgroundColor: ['rgba(251, 236, 81, 0.5)'],
                                         borderWidth: 1,
                                     },
@@ -40,17 +33,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         });
                     }
 
-                    if (perso) {
+                    if (data.perso) {
                         const ctxPerso = document.getElementById('myChartPerso')
                             .getContext('2d');
                         const myChartPerso = new Chart(ctxPerso, {
                             type: 'radar',
                             data: {
-                                labels: perso.ability,
+                                labels: data.perso.ability,
                                 datasets: [
                                     {
                                         label: 'Notes',
-                                        data: perso.score,
+                                        data: data.perso.score,
                                         backgroundColor: ['rgba(101, 141, 251, 0.5)'],
                                         borderWidth: 1,
                                     },
