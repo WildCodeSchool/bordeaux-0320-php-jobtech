5d0a64d47deb4b1af9eab0228ed894f31917408d
francoisvaillant <frvaillant@gmail.com>
2020-07-29 23:51:15 +0200

    es6

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -2,19 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const persons = document.getElementsByClassName('contact-line');
 
     if (persons) {
-        for (let i = 0; i < persons.length; i+=1) {
+        for (let i = 0; i < persons.length; i += 1) {
             persons[i].addEventListener('click', () => {
-                const user = persons[i].dataset.user;
+                const { user } = persons[i].dataset;
                 persons[i].innerHTML += '<span class="small">chargement ...</span>';
-                const url = '/messagerie/reset/' + user;
+                const url = `/messagerie/reset/${user}`;
                 fetch(url)
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(json => {
-                        window.location.href = '/messagerie/admin/' + user;
-                    })
-            })
+                    .then(response => response.json())
+                    .then((json) => {
+                        window.location.href = `/messagerie/admin/${user}`;
+                    });
+            });
         }
     }
 })
