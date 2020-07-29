7efd634b0b6418606824895ba888dc7482e0c07d
francoisvaillant <frvaillant@gmail.com>
2020-07-29 23:39:59 +0200

    add semicolon in js

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const persons = document.getElementsByClassName('contact-line')
+    const persons = document.getElementsByClassName('contact-line');
 
     if (persons) {
         for (let i = 0; i < persons.length; i++) {
             persons[i].addEventListener('click', () => {
-                const user = persons[i].dataset.user
-                persons[i].innerHTML += '<span class="small">chargement ...</span>'
-                const url = '/messagerie/reset/' + user
+                const user = persons[i].dataset.user;
+                persons[i].innerHTML += '<span class="small">chargement ...</span>';
+                const url = '/messagerie/reset/' + user;
                 fetch(url)
                     .then(response => {
-                        return response.json()
+                        return response.json();
                     })
                     .then(json => {
-                        window.location.href = '/messagerie/admin/' + user
+                        window.location.href = '/messagerie/admin/' + user;
                     })
             })
         }
