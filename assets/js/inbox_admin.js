b5fef7b6f644de4f087f281c09132575f99b97c3
TimoArdi <t.ardilouze@gmail.com>
2020-07-29 17:42:05 +0200

    Final Messagerie , without CSS

--- assets/js/inbox_admin.js
+++ assets/js/inbox_admin.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const contacts = document.getElementsByClassName('contact-box');
     for (let i = 0; i < contacts.length; i += 1) {
         contacts[i].addEventListener('click', ()=>{
-            console.log();
-        })
+            const url = contacts[i].getAttribute('data-url');
+            window.location.href = url;
+        });
     }
 })
--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Candidate;
 use App\Entity\Message;
+use App\Entity\User;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,14 +23,28 @@ class MessageType extends AbstractType
             ])
             ->add('content', CKEditorType::class, [
                 'label' => 'Message :'
-            ])
-        ;
+            ]);
+        if ($options['admin'] ===  true) {
+            $builder
+                ->add('contact', EntityType::class, [
+                    'label' => 'Utilisateur : ',
+                    'class' => User::class,
+                    'choice_label' => function (User $user) {
+                        if ($user->getCandidate() !== null) {
+                            return $user->getCandidate()->getFullName();
+                        } else {
+                            return $user->getCompany()->getName();
+                        }
+                    }
+                ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Message::class,
+            'admin' => false,
         ]);
     }
 }
