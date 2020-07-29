59e4d60ed35391c0d66c90a4b1bb8f6e6acee01c
Ludovic Dureau <ludodureau@gmail.com>
2020-07-29 14:57:13 +0200

    UTF8 fixed

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/actualités", name="news_")
+ * @Route("/actualites", name="news_")
  */
 class NewsController extends AbstractController
 {
